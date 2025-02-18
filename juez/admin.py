27716282bb47f3a95d8e29cea7a50399bf988c2f
Lukasv99 <lukassv@hotmail.cl>
2025-02-17 22:12:49 -0300

    Enunciado

--- juez/admin.py
+++ juez/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Problema  # Solo importamos Problema
+from .models import Problema
 
-admin.site.register(Problema)
+class ProblemaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descripcion')  # Solo mostramos título y descripción en la lista
+    search_fields = ('titulo',)
+    list_filter = ('titulo',)
+
+admin.site.register(Problema, ProblemaAdmin)
--- juez/models.py
+++ juez/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Problema(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
-    entrada_ejemplo = models.TextField()
-    salida_ejemplo = models.TextField()
-    archivo_pruebas = models.FileField(upload_to='pruebas/', blank=True, null=True)
+    enunciado = models.TextField(default="Enunciado no disponible")  # Agregamos el enunciado
+    entrada_ejemplo = models.TextField(blank=True, null=True)
+    salida_ejemplo = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.titulo
